0ecbef3a7eaa8d4363e7d6fa40091a7ea06ed089
umapathy1729 <umapathy428@gmail.com>
2026-07-23 10:57:21 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "dev-aman-tf-bucket"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
